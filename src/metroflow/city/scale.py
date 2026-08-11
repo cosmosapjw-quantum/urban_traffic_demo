50182e4c7f0712e814177d5a153d17837d50ddd1
cosmosapjw-quantum <cosmosapjw@gmail.com>
2026-08-11 11:07:44 +0900

    feat(city): normalize CityScaleSpec values

--- src/metroflow/city/scale.py
+++ src/metroflow/city/scale.py
@@ -7,3 +7,7 @@ __all__ = ["CityScaleSpec"]
 class CityScaleSpec:
     target_population: int
     urbanized_area_km2: float
+
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "target_population", int(self.target_population))
+        object.__setattr__(self, "urbanized_area_km2", float(self.urbanized_area_km2))
