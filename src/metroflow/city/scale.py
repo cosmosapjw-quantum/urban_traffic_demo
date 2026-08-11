6d427d193c4b6e5623b0f6e299385e19f850243f
cosmosapjw-quantum <cosmosapjw@gmail.com>
2026-08-11 11:08:14 +0900

    feat(city): validate population authority

--- src/metroflow/city/scale.py
+++ src/metroflow/city/scale.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from numbers import Integral
 
 __all__ = ["CityScaleSpec"]
 
@@ -9,5 +10,9 @@ class CityScaleSpec:
     urbanized_area_km2: float
 
     def __post_init__(self) -> None:
+        if isinstance(self.target_population, bool) or not isinstance(
+            self.target_population, Integral
+        ):
+            raise TypeError("population must be a non-bool integral value")
         object.__setattr__(self, "target_population", int(self.target_population))
         object.__setattr__(self, "urbanized_area_km2", float(self.urbanized_area_km2))
